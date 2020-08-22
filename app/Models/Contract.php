3a4347ea3d8e2941d640f5f3a8645d08a0d08ae4
abdelrahman osama <eslamboully@gmail.com>
2020-08-23 00:08:20 +0200

    update

---
+++ app/Models/Contract.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contract extends Model
+{
+    protected $fillable = ['description','job_id','accept','employee_id','refusal_details'];
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class,'job_id','id');
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class,'employee_id','id');
+    }
+}
---
+++ database/migrations/2020_08_22_215939_create_contracts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->text('description');
+            $table->text('refusal_details')->nullable();
+            $table->tinyInteger('accept')->default(0);
+
+            $table->unsignedBigInteger('job_id');
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
+
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
